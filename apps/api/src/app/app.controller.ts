ed02acf79d21e166115c6748b7c92d55212bedbb
RafalKr81 <krolrafal@gmail.com>
2021-10-31 16:48:48 +0100

    added auth API decorators

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,16 +1,14 @@
 import { Message } from '@archives/api-interfaces';
 import { Controller, Get } from '@nestjs/common';
-import { Auth } from '../auth.decorator';
+import { Auth } from './decorators/auth.decorator';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @UseGuards(AuthGuard('jwt'), PermissionsGuard)
-  // @Permissions('read:hello')
   @Get('hello')
-  @Auth('read:hello')
+  @Auth('read:hello', 'hello')
   getData(): Message {
     return this.appService.getData();
   }
